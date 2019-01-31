415f6c96e41b2c61a46ade98da9a1af260526d1b
eliasmpw <eliasmpw@hotmail.com>
2019-02-01 00:14:05 +0100

    add styles and click event for filters

--- assets/js/filters.js
+++ assets/js/filters.js
@@ -1,4 +1,5 @@
 function setFilterEvents() {
+    document.getElementById("land-use-button").addEventListener("click", handleLandUseButtonClick);
     document.getElementById("check-agriculture").addEventListener("click", handleCheckboxClick);
 
 }
@@ -6,4 +7,18 @@ function setFilterEvents() {
 function handleCheckboxClick(event) {
     console.log(event);
     console.log(event.target.dataset);
+}
+
+function handleLandUseButtonClick(event) {
+    switch (document.getElementById("checkboxes-container").style.visibility) {
+        case "visible":
+            document.getElementById("checkboxes-container").style.visibility = "hidden";
+            break;
+        case "hidden":
+            document.getElementById("checkboxes-container").style.visibility = "visible";
+            break;
+        default:
+            document.getElementById("checkboxes-container").style.visibility = "visible";
+            break;
+    }
 }
