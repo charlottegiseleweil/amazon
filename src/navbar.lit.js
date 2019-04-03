f5190755a4f24ecadeeda8c775106236578c9db0
Ian Campbell <iancam92@icloud.com>
2019-04-02 17:17:58 -0700

    fixed an issue where sources weren't being appropriately loaded

--- src/navbar.lit.js
+++ src/navbar.lit.js
@@ -1,5 +1,5 @@
 import { dropdown as menu } from "./menu.lit.js";
-import { link } from "./link.lit.js/index.js";
+import { link } from "./link.lit.js";
 import { classFromProps } from "./utilities.js";
 import { navbar as d_navbar } from "./menuData.js";
 import { html, render } from "../node_modules/lit-html/lit-html.js";
