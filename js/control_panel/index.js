d0cf4836f60cba73c230f4d92e809362d467d7b8
Ian Campbell <iancam92@icloud.com>
2019-04-14 11:03:55 -0700

    switching to development

--- js/control_panel/index.js
+++ js/control_panel/index.js
@@ -4,54 +4,13 @@ import { navbar } from "../menuData.js";
 import { classify, withState } from "../utilities.js";
 import { classMap } from "../../lib/js/lit-html/directives/class-map.js";
 
-const tabs = [
-  ["servicios ecosistemicos",
-    html`
-
-
-      <style>
-        .p{
-        display:block;
-      }
-      </style>
-
-
-      <a href='work in progress'>Descargar resultados</a>
-
-
-
-
-
-      <!-- Below not working and this is really sad! Ian help?-->
-      <script src='lib/js/includehtml.js'></script>
-      <div w3-include-html='test.html'></div>
-      <script>includeHTML();</script>
-    `
-  ],
-  ["modelos", "Coming soon ... Metodologias etc y enlaces a InVEST"],
-  [
-    "escenarios",
-    html`
-      <img src="static/images/desarollando_escenarios.JPG" height=200px alt="Img" />
-      <p> Desarollando Escenarios </p>
-      <img src="static/images/desarollando_escenarios1.JPG" height=200px alt="Img" />
-    `
-  ]
-].map(([title, displayContent], i) => ({
-  onClick: (setState, state) => () => {
-    setState({ displayContent, selected: i });
-  },
-  title,
-  displayContent
-}));
-
 const tabContainerStyle = "bg-black br2 white flex justify-center avenir";
-const tabStyle = (i, selected) =>
+const defaultTabStyle = (i, selected) =>
   "bt br pa3 bg-black fl tc ttc link bg-animate hover-bg-dark-gray pointer" +
   (i === 0 ? " bl" : "") +
   (i === selected ? "" : " bb");
 
-const control_panel = ({
+export const control_panel = ({
   tabs = [],
   state: { displayContent, selected },
   setState
@@ -62,7 +21,7 @@ const control_panel = ({
         return html`
           <a
             @click=${t.onClick(setState, displayContent)}
-            class=${classMap(classify(tabStyle(i, selected)))}
+            class=${classMap(classify(defaultTabStyle(i, selected)))}
           >
             <div>${t.title}</div>
           </a>
@@ -74,23 +33,3 @@ const control_panel = ({
     </div>
   `;
 };
-
-// (<div id="control_panel">
-//   <div class="bg-black white flex justify-center">
-//     <div class="br bt bb bl pa2 bg-black fl tc">
-//       servicios ecosistemicos (Descargar Resultados)
-//     </div>
-//     <div class="bt bb br pa2 bg-black fl tc">modelos y metodologia</div>
-//     <div class="bt bb br pa2 bg-black fl tc">co-desarrollo de escenarios</div>
-//   </div>
-//   <div class="bg-black white pa3 flex justify-center">
-//     lot's of other content, like where to download data and so on
-//   </div>
-// </div>;)
-
-withState({ displayContent: tabs[0].displayContent, selected: 0 })(
-  control_panel,
-  {
-    tabs
-  }
-)(document.getElementById("control_panel"));
