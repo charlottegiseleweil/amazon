cf3c81aba5077c7ebf73efc712e1674ce830aac2
Anna <annfr542@student.liu.se>
2020-04-07 10:14:36 +0200

    new data historia

--- js/maps_historia.js
+++ js/maps_historia.js
@@ -14,7 +14,7 @@ var basemap = L.tileLayer(
 
 var map = L.map("mapa", {
   layers: [basemap],
-  center: [-11.0058, -68.74],
+  center: [-11.0067, -68.746],
   zoom: 14
 });
 
@@ -62,17 +62,18 @@ shapefileLayer("CBE_flood_2015","mapa6",floodBaseStyle);
 shapefileLayer("I_flood_2015","mapa12",floodBaseStyle);
 
 shapefileLayer("CBE_flood_levels_0","Nivel_179msnm",floodBaseStyle);
-shapefileLayer("I_flood_levels_0","mapa12",floodBaseStyle);
+shapefileLayer("I_flood_levels_0","I_1metro",floodBaseStyle);
 shapefileLayer("CBE_flood_levels_1","Nivel_181msnm",floodBaseStyle);
-shapefileLayer("I_flood_levels_0","mapa12",floodBaseStyle);
+shapefileLayer("I_flood_levels_1","I_4metros",floodBaseStyle);
 shapefileLayer("CBE_flood_levels_2","Nivel_186msnm",floodBaseStyle);
-shapefileLayer("I_flood_levels_1","mapa12",floodBaseStyle);
+shapefileLayer("I_flood_levels_2","I_6metros",floodBaseStyle);
 shapefileLayer("CBE_flood_levels_3","Nivel_188msnm",floodBaseStyle);
-shapefileLayer("I_flood_levels_2","mapa12",floodBaseStyle);
+shapefileLayer("I_flood_levels_3","I_8metros",floodBaseStyle);
 shapefileLayer("CBE_flood_levels_4","Nivel_190msnm",floodBaseStyle);
-shapefileLayer("I_flood_levels_3","mapa12",floodBaseStyle);
+shapefileLayer("I_flood_levels_4","I_10metros",floodBaseStyle);
 shapefileLayer("CBE_flood_levels_5","Nivel_192msnm",floodBaseStyle);
-shapefileLayer("I_flood_levels_3","mapa12",floodBaseStyle);
+shapefileLayer("I_flood_levels_5","I_12metros",floodBaseStyle);
+shapefileLayer("CBE_flood_levels_6","Nivel_193.5msnm",floodBaseStyle);
 
 
 
@@ -93,17 +94,18 @@ function changeArea(area){
 
   switch(area) {
     case "CBE":
-      map.panTo(new L.LatLng(-11.0058, -68.74));
+      map.panTo(new L.LatLng(-11.0067, -68.746));
       document.getElementById("mapText").innerHTML = 'En Cobija, capital del Departamento Pando, Bolivia, había para esas fechas 2,100 personas instaladas en 12 albergues. (ONU 2015). Otro tanto sucedió en las ciudades de Brasiléia y Epitaciolândia, Acre, Brasil, donde los niveles de inundación superaron los 13 m como dan cuenta diversas fuentes informativas. (El Comercio, América Noticias, Acre Noticias, Reliefweb, Ejutv, RPP Noticias, Acre Alerta, Agencia Brasil, Folha de S. Paulo, O alto Acre). <br><br> Esta gran inundación, considerada “histórica” en la región, permitió una aproximación de estudio de caso, que culminó en la elaboración de una base de datos en un sistema de información geográfica (SIG). Este instrumento ilustra los niveles que alcanzó el agua' 
       break;
     default: // Iñapari
       map.panTo(new L.LatLng(-10.94, -69.57));
       document.getElementById("mapText").innerHTML = 'En la localidad de Iñapari, Madre de Dios, Perú, el 19 de febrero de ese año, aconsecuencia de intensas lluvias, se registró el incremento del caudal de los ríos Acre y Yaverija. Posteriormente, el desborde de ambos ríos inundó viviendas, locales públicos, vías de comunicación y áreas de cultivo en el distrito y ciudad de Iñapari. Para el día 24 se tenía 1.050 damnificados y más de 240 viviendas dañadas. (INDECI/COEN 2015).  <br><br> Esta gran inundación, considerada “histórica” en la región, permitió una aproximación de estudio de caso, que culminó en la elaboración de una base de datos en un sistema de información geográfica (SIG). Este instrumento ilustra los niveles que alcanzó el agua' 
-  }
+    }
 
   let type = $('input[name=type]:checked').val();
   if(type == "flood_levels"){
     layers[area+"_"+type+"_"+document.getElementById("flood_range").value].addTo(map);
+    document.getElementById("flood_range").max = (area == "CBE") ? 6:5;
   }
   else{
     layers[area+"_"+type].addTo(map);
@@ -158,11 +160,18 @@ floodRangeSlider.onAdd = function (map) {
 };
 
 floodRangeSlider.update = function (props) {
-    this._div.innerHTML = '<div id="slider" ><p id="slider_value"><span id="flood_m">8</span> m</p><input type="range" min="0" max="5" value="2" class="slider" id="flood_range"></div>';
+    let area = $('input[name=area]:checked').val();
+    if (area == "CBE"){
+      this._div.innerHTML = '<div id="slider" ><p id="slider_value"><span id="flood_m">8</span> m</p><input type="range" min="0" max="6" value="2" class="slider" id="flood_range"></div>';
+    }
+    else{
+      this._div.innerHTML = '<div id="slider" ><p id="slider_value"><span id="flood_m">8</span> m</p><input type="range" min="0" max="5" value="3" class="slider" id="flood_range"></div>';
+    }
 };
 
 function changeFloodLevel(value){
-  let flood_levels = [1,3,8,10,12,14];
+  let area = $('input[name=area]:checked').val();
+  let flood_levels = (area=="CBE") ? [1,3,8,10,12,14,15.5]: [1,4,6,8,10,12] ;
   var output = document.getElementById("flood_m");
   output.innerHTML = flood_levels[value];
 
@@ -175,18 +184,13 @@ function changeFloodLevel(value){
   });
   labels.addTo(map);
 
-  let area = $('input[name=area]:checked').val();
-
   layers[area+"_flood_levels_" + value].addTo(map);
-
-
 }
 
 
 // - - - - - - - -
 // Initialization
 // - - - - - - - - 
-
 
 // ---
 $( document ).ready(function() {
